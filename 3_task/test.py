c28d26b066402c8234f9fee39073cd87e7671ef8
Anton Artikov <arti-anton@yandex.ru>
2024-09-08 20:52:12 +0300

    new task

---
+++ 3_task/test.py
@@ -0,0 +1,20 @@
+import pytest
+
+from main import format_phone
+
+STANDARD = "8 (901) 123-45-67"
+
+@pytest.mark.parametrize(
+    'input_phone, result_phone',
+    [
+        ("89011234567", STANDARD),
+        ("9011234567", STANDARD),
+        ("79011234567", STANDARD),
+        ("+79011234567", STANDARD),
+        ("8 __()-! 901-123-45-67", STANDARD),
+        ("+7901-123-45   67", STANDARD),
+        ("#@!(zz8901-___123-45gg67 какая-то ещё петрушка R$&*z", STANDARD)
+    ]
+)
+def test_format_phone(input_phone, result_phone):
+    assert format_phone(input_phone) == result_phone
