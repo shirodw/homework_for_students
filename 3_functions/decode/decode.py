3d507ecb107a52815677cc8bc70f7a8a1e26f5bc
Anton Artikov <arti-anton@yandex.ru>
2024-09-15 14:35:51 +0300

    добавил задание для функций

---
+++ 3_functions/decode/decode.py
@@ -0,0 +1,4 @@
+def decode_numbers(numbers: str) -> str | None:
+    """Пишите ваш код здесь."""
+    result = ""
+    return result
---
+++ 3_functions/encode/encode.py
@@ -0,0 +1,4 @@
+def encode_text(text: str) -> str | None:
+    """Пишите ваш код здесь."""
+    result = ""
+    return result
