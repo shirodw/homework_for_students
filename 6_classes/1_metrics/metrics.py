8e772da7844d70afc12bd5c66edc94c0fa6f57ab
Anton Artikov <arti-anton@yandex.ru>
2024-09-21 22:34:26 +0300

    добавил задачу по классам

---
+++ 6_classes/1_metrics/metrics.py
@@ -0,0 +1,14 @@
+class Statsd:
+    def __init__(self):
+        """Реализуйте класс"""
+
+
+def get_txt_statsd(path: str, buffer_limit: int = 10) -> Statsd:
+    """Реализуйте инициализацию метрик для текстового файла"""
+    pass
+
+
+def get_csv_statsd(path: str, buffer_limit: int = 10) -> Statsd:
+    """Реализуйте инициализацию метрик для csv файла"""
+    pass
+
