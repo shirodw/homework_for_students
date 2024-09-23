878f3fb783464cb64967ee613c9a5213a6355c87
UrakadakiRemuru <met-zi@mail.ru>
2024-09-23 12:09:56 +0300

    2_sentence_is_pangram done

--- 1_strings/2_sentence_is_pangram/main.py
+++ 1_strings/2_sentence_is_pangram/main.py
@@ -10,3 +10,12 @@ pytest ./2_sentence_is_pangram/test.py
 
 def is_sentence_is_pangram(sentence: str) -> bool:
     """Пишите ваш код здесь."""
+    marker = True
+    help_str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.lower()
+    sentence = sentence.lower()
+
+    for symbol in help_str:
+        if symbol not in sentence:
+            return False
+
+    return True
